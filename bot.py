6b295a558d63e251c34361836d79dd027d9ded5a
Anji <133198103+anjikey@users.noreply.github.com>
2025-03-04 23:29:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,33 +1,32 @@
-import asyncio
+import os
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
 
-# Твой токен бота (лучше хранить в переменных окружения)
-import os
+# Твой токен бота
 TOKEN = os.getenv("TOKEN")
-
 
 # Логирование
 logging.basicConfig(level=logging.INFO)
 
 # Создание бота и диспетчера
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
+dp = Dispatcher(bot)
 
 # Клавиатура
 keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-buttons = [KeyboardButton("📝 Заполнить дневник"), KeyboardButton("🎯 Цели"), KeyboardButton("🏋️‍♂️ Спорт")]
+buttons = [KeyboardButton("🎯 Цели"), KeyboardButton("⚽ Спорт")]
 keyboard.add(*buttons)
 
 # Обработчик команды /start
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.answer("Привет! Я твой коуч-бот по саморазвитию. Чем займемся сегодня?", reply_markup=keyboard)
+    await message.answer("Привет! Я твой коуч по саморазвитию. Чем займемся сегодня?", reply_markup=keyboard)
 
 # Запуск бота
 async def main():
-    await dp.start_polling(bot)
+    await dp.start_polling()
 
 if __name__ == "__main__":
     asyncio.run(main())
